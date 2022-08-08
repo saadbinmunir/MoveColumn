f867dfcd301ff3656f5bf1a3c768470142ef1688
Saad <saadmunir24@gmail.com>
2022-08-08 19:06:41 +0100

    upload all changes

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from movecolumn import __version__
+
+setup(
+    name='movecolumn',
+    version=__version__,
+    description = "A creative package to move columns in Python dataframes.",
+    long_description = '' ,
+    url='https://github.com/saadbinmunir/Calendric-lib.git',
+    author='Saad Bin Munir',
+    author_email='saadmunir24@gmail.com',
+    packages=find_packages(),
+)
